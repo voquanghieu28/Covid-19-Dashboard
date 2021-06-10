d00e107794ed2ad8643a3b43637ef8559119407d
Quang Hieu Vo <voquanghieu28@gmail.com>
2021-06-10 11:19:00 -0400

    Add files via upload

---
+++ build/precache-manifest.beb05aebcdefde184f63c53e5fc4ce7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1800cfc5ed9190f474fa4163639f6e58",
+    "url": "/Covid-Dashboard/index.html"
+  },
+  {
+    "revision": "92b93dec87f524a1cb51",
+    "url": "/Covid-Dashboard/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9d74de9891b21f2bf18c",
+    "url": "/Covid-Dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92b93dec87f524a1cb51",
+    "url": "/Covid-Dashboard/static/js/2.5f0ae728.chunk.js"
+  },
+  {
+    "revision": "26eaf80254b52f2ebcf3542565f78969",
+    "url": "/Covid-Dashboard/static/js/2.5f0ae728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d74de9891b21f2bf18c",
+    "url": "/Covid-Dashboard/static/js/main.4d63a8f1.chunk.js"
+  },
+  {
+    "revision": "d61ac815dfb70df50a41",
+    "url": "/Covid-Dashboard/static/js/runtime-main.9bd422c8.js"
+  }
+]);
